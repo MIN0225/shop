70fe1286ad29179c6e29be1c486fc1ea45d2f163
MIN0225 <cro77@naver.com>
2022-12-14 21:24:49 +0900

    5.1  연관 관계 매핑 종류
    5.1.3 다대일/일대다 양방향 매핑하기
    5.1.4 다대다 매핑하기(해보진 않고 보기만)

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,39 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders") // 정렬할 때 사용되는 "order" 키워드가 있기 때문에 Order 엔티티에 매핑되는 테이블로 "orders"를 지정한다.
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문상태
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}
