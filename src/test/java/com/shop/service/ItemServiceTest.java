2df678e4eedbc0f731cb2a281eec2ca580356b98
MIN0225 <cro77@naver.com>
2022-12-18 11:43:31 +0900

    6.1 상품 등록하기

---
+++ src/test/java/com/shop/service/ItemServiceTest.java
@@ -0,0 +1,77 @@
+package com.shop.service;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
+import com.shop.entity.Item;
+import com.shop.entity.ItemImg;
+import com.shop.repository.ItemImgRepository;
+import com.shop.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ItemServiceTest {
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    ItemImgRepository itemImgRepository;
+
+    List<MultipartFile> createMultipartFiles() throws Exception{  // MockMultipartFile 클래스를 이용하여 가짜 MultipartFile 리스트를 만들어서 반환해주는 메소드.
+        List<MultipartFile> multipartFileList = new ArrayList<>();
+
+        for(int i=0; i<5; i++){
+            String path = "/Users/macbookpro/Desktop/shop/item";
+            String imageName = "image" + i + ".jpg";
+            MockMultipartFile multipartFile = new MockMultipartFile(path, imageName, "image/jpg", new byte[]{1,2,3,4});
+            multipartFileList.add(multipartFile);
+        }
+        return multipartFileList;
+    }
+
+    @Test
+    @DisplayName("상품 등록 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void saveItem() throws Exception{
+        ItemFormDto itemFormDto = new ItemFormDto();
+        itemFormDto.setItemNm("테스트상품");
+        itemFormDto.setItemSellStatus(ItemSellStatus.SELL);
+        itemFormDto.setItemDetail("테스트 상품 입니다.");
+        itemFormDto.setPrice(1000);
+        itemFormDto.setStockNumber(100);
+
+        List<MultipartFile> multipartFileList = createMultipartFiles();
+        //System.out.println("multipartFileList.get(0): " + multipartFileList.get(0).getOriginalFilename());
+        Long itemId = itemService.saveItem(itemFormDto, multipartFileList);
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        //System.out.println("itemImgList.get(0).getOriImgName(): " + itemImgList.get(0).getOriImgName());
+        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+
+        assertEquals(itemFormDto.getItemNm(), item.getItemNm()); // 입력한 상품 데이터와 실제로 저장된 상품 데이터가 같은지 확인
+        assertEquals(itemFormDto.getItemSellStatus(), item.getItemSellStatus());
+        assertEquals(itemFormDto.getItemDetail(), item.getItemDetail());
+        assertEquals(itemFormDto.getPrice(), item.getPrice());
+        assertEquals(itemFormDto.getStockNumber(), item.getStockNumber());
+        assertEquals(multipartFileList.get(0).getOriginalFilename(), itemImgList.get(0).getOriImgName()); // 상품 이미지는 첫 번째 파일의 원본 이미지 파일 이름만 같은지 확인
+
+    }
+
+}
