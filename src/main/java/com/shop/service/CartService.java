cab17386f90c34147ba615be71ed7e6ac3c30100
MIN0225 <cro77@naver.com>
2022-12-26 20:57:33 +0900

    8.3
    p.363까지

--- src/main/java/com/shop/service/CartService.java
+++ src/main/java/com/shop/service/CartService.java
@@ -21,6 +21,9 @@ import java.util.List;
 
 import org.thymeleaf.util.StringUtils;
 
+import com.shop.dto.CartOrderDto;
+import com.shop.dto.OrderDto;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -30,6 +33,7 @@ public class CartService {
     private final MemberRepository memberRepository;
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final OrderService orderService;
 
     public Long addCart(CartItemDto cartItemDto, String email){
 
@@ -92,6 +96,31 @@ public class CartService {
     public void deleteCartItem(Long cartItemId){
         CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
         cartItemRepository.delete(cartItem);
+    }
+
+    public Long orderCartItem(List<CartOrderDto> cartOrderDtoList, String email){
+        List<OrderDto> orderDtoList = new ArrayList<>();
+        for(CartOrderDto cartOrderDto : cartOrderDtoList){
+            CartItem cartItem = cartItemRepository
+                    .findById(cartOrderDto.getCartItemId())
+                    .orElseThrow(EntityNotFoundException::new);
+
+            OrderDto orderDto = new OrderDto();
+            orderDto.setItemId(cartItem.getItem().getId());
+            orderDto.setCount(cartItem.getCount());
+            orderDtoList.add(orderDto);
+        }
+
+        Long orderId = orderService.orders(orderDtoList, email);  // 장바구니에 담은 상품을 주문하도록 주문 로직을 호출
+
+        for(CartOrderDto cartOrderDto : cartOrderDtoList){  // 주문한 상품들을 장바구니에서 제거.
+            CartItem cartItem = cartItemRepository
+                    .findById(cartOrderDto.getCartItemId())
+                    .orElseThrow(EntityNotFoundException::new);
+            cartItemRepository.delete(cartItem);
+        }
+
+        return orderId;
     }
 
 }
